32d52fbd0b8dc985f09f2bb632bb80e91de36109
SinisaPaskulov <sinishamarini@gmail.com>
2019-03-06 14:12:01 +0100

    Dodane poslednje cetiri aktivnosti(Jovan)

---
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/ContactActivity.java
@@ -0,0 +1,44 @@
+package com.example.pmsu_2019_projekat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pmsu_2019_projekat.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
