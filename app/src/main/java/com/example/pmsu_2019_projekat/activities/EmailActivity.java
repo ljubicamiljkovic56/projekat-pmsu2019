237497ade915954f6c8fca4639de8daf518d2e7f
SinisaPaskulov <sinishamarini@gmail.com>
2019-03-06 15:19:04 +0100

    Napravljene pocetne aktivnosti

---
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/EmailActivity.java
@@ -0,0 +1,47 @@
+package com.example.pmsu_2019_projekat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pmsu_2019_projekat.R;
+
+import static com.example.pmsu_2019_projekat.R.*;
+
+public class EmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_email);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
