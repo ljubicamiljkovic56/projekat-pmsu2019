de8429e33afd808f497e616f3ac1ed1d1d5b0036
SinisaPaskulov <sinishamarini@gmail.com>
2019-03-08 10:50:56 +0100

    Pokretanje logina iz splashActivity i login dugme

---
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.pmsu_2019_projekat.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pmsu_2019_projekat.R;
+
+import static com.example.pmsu_2019_projekat.R.*;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_login);
+    }
+
+    public void btnStartEmailsActivity(View v){
+        Intent i = new Intent(LoginActivity.this, EmailsActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/example/pmsu_2019_projekat/activities/SplashActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +12,46 @@ import static com.example.pmsu_2019_projekat.R.*;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_splash);
+
+        new InitTask().execute();
+    }
+
+    private class InitTask extends AsyncTask<Void, Void, Void>
+    {
+        private long startTime;
+
+        @Override
+        protected void onPreExecute()
+        {
+            startTime = System.currentTimeMillis();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0)
+        {
+            startMainActivity();
+
+            return null;
+        }
+
+        private void continueLogin()
+        {
+            long timeLeft = SPLASH_TIME_OUT - (System.currentTimeMillis() - startTime);
+            if(timeLeft < 0) timeLeft = 0;
+            SystemClock.sleep(timeLeft);
+        }
+    }
+
+    private void startMainActivity()
+    {
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        finish();
     }
 
     @Override
