000c4caec8850f04e32c5408312c642f48b38aca
SinisaPaskulov <sinishamarini@gmail.com>
2019-03-21 12:21:51 +0100

    Izmenjen settings activity

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/SettingsActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/SettingsActivity.java
@@ -1,16 +1,45 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.pmsu_2019_projekat.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 
     @Override
