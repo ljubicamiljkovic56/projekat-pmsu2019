03b4f0ec1f3fee69c0412c343c79794325e36367
ljubica_m56 <ljubica.m19@gmail.com>
2019-03-24 16:55:49 +0100

    U aktivnosti FolderActivity dodat je toolbar sa edit dugmetom i u aktivnosti CreateFolderActivity dodat je toolbar sa save i cancel dugmetom.

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/FolderActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/FolderActivity.java
@@ -1,17 +1,67 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.pmsu_2019_projekat.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FolderActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folder);
+
+        toolbar = (Toolbar) findViewById(R.id.folder_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Toolbar");
+        toolbar.setSubtitle("Folder Activity");
+
+        List<String> list = new ArrayList<>();
+        list.add("stavka 1");
+        list.add("stavka 2");
+        list.add("stavka 3");
+        list.add("stavka 4");
+        list.add("stavka 5");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,list);
+        ListView listView = (ListView) findViewById(R.id.folder_list_view);
+        listView.setAdapter(arrayAdapter);
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.folder_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        String message = "";
+        switch (item.getItemId()){
+            case R.id.toolbar_edit:
+                message = "Edit";
+                break;
+        }
+        Toast.makeText(this, message + "  selected", Toast.LENGTH_LONG).show();
+        return  super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onStart() {
