584920a3515c252d50d8950a0294b4910d74775a
ljubica_m56 <ljubica.m19@gmail.com>
2019-03-24 16:53:30 +0100

    U aktivnosti FolderActivity dodat je toolbar sa edit dugmetom i u aktivnosti CreateFolderActivity dodat je toolbar sa save i cancel dugmetom.

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateFolderActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateFolderActivity.java
@@ -1,16 +1,54 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 import com.example.pmsu_2019_projekat.R;
 
 public class CreateFolderActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_folder);
+
+        toolbar = (Toolbar) findViewById(R.id.create_folder_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Toolbar");
+        toolbar.setSubtitle("Create Folder Activity");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_folder_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        String message = "";
+        switch (item.getItemId()){
+            case R.id.toolbar_save:
+                message = "Save";
+                break;
+            case R.id.toolbar_cancel:
+                message = "Cancel";
+                break;
+        }
+        Toast.makeText(this, message + "  selected", Toast.LENGTH_LONG).show();
+        return  super.onOptionsItemSelected(item);
     }
 
     @Override
