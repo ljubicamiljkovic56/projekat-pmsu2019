0549ffce752649b6ebc2597d06f7991e843b0532
Despot <jovan1998despotov@gmail.com>
2019-03-26 15:30:52 +0100

    jos samo slika u splashu ostala...

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/ContactActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/ContactActivity.java
@@ -2,6 +2,9 @@ package com.example.pmsu_2019_projekat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.pmsu_2019_projekat.R;
 
@@ -11,6 +14,18 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.contact_activity_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Toolbar");
+        toolbar.setSubtitle("Profile Activity");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contact_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
     @Override
     protected void onStart() {
--- app/src/main/java/com/example/pmsu_2019_projekat/activities/ProfileActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/ProfileActivity.java
@@ -2,6 +2,9 @@ package com.example.pmsu_2019_projekat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.pmsu_2019_projekat.R;
 
@@ -11,6 +14,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profile_activity_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Toolbar");
+        toolbar.setSubtitle("Profile Activity");
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
