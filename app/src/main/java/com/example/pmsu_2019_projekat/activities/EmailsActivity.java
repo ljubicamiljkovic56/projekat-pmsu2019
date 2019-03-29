8705caba9c2054a3cde09e2277c7f62db3381866
SinisaPaskulov <sinishamarini@gmail.com>
2019-03-29 10:52:04 +0100

    Navigation Drawer je 60% gotov

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/EmailsActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/EmailsActivity.java
@@ -1,8 +1,12 @@
 package com.example.pmsu_2019_projekat.activities;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,17 +16,33 @@ import static com.example.pmsu_2019_projekat.R.*;
 
 public class EmailsActivity extends AppCompatActivity {
 
+
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_emails);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.emails_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Emails Activity");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(drawable.ic_menu_white_24dp);
+        drawerLayout = findViewById(id.navigation_drawer);
     }
 
-/*    public void btnStartCreateEmailActivity(View v){
-        Intent i = new Intent(EmailsActivity.this, CreateEmailActivity.class);
-        startActivity(i);
-        finish();
-    }*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
+
 
     public void onButtonClicked(View v){
 
