9b488ed9521ddec22de65317b2457b3e069923c8
ljubica_m56 <ljubica.m19@gmail.com>
2019-04-01 20:27:56 +0200

    U FoldersActivity je dodat NavigationDrawer i Toolbar.

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/FoldersActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/FoldersActivity.java
@@ -1,19 +1,77 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
 
 import com.example.pmsu_2019_projekat.R;
 
-public class FoldersActivity extends AppCompatActivity {
+
+public class FoldersActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folders);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.folders_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Folders Activity");
+
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setHomeButtonEnabled(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.folders_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.folders_create) {
+            Intent a = new Intent(FoldersActivity.this, CreateFolderActivity.class);
+            startActivity(a);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+@Override
     protected void onStart() {
         super.onStart();
     }
@@ -41,5 +99,26 @@ public class FoldersActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_folders) {
+        } else if (id == R.id.nav_contacts) {
+            Intent c = new Intent(FoldersActivity.this, ContactsActivity.class);
+            startActivity(c);
+        } else if (id == R.id.nav_emails) {
+            Intent d = new Intent(FoldersActivity.this, FoldersActivity.class);
+            startActivity(d);
+        } else if (id == R.id.nav_settings) {
+            Intent f = new Intent(FoldersActivity.this, SettingsActivity.class);
+            startActivity(f);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
