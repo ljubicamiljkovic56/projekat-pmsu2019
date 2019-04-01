9513a33c1f8f6a3e308e21832996a9d040435585
SinisaPaskulov <sinishamarini@gmail.com>
2019-04-01 18:51:42 +0200

    Navigation Drawer je implementiran u emails activity

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/EmailsActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/EmailsActivity.java
@@ -1,11 +1,15 @@
 package com.example.pmsu_2019_projekat.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +18,8 @@ import com.example.pmsu_2019_projekat.R;
 
 import static com.example.pmsu_2019_projekat.R.*;
 
-public class EmailsActivity extends AppCompatActivity {
-
-
-    private DrawerLayout drawerLayout;
+public class EmailsActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +29,53 @@ public class EmailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.emails_toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Emails Activity");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(drawable.ic_menu_white_24dp);
-        drawerLayout = findViewById(id.navigation_drawer);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(drawable.ic_menu_white_24dp);
+        actionBar.setHomeButtonEnabled(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                break;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-        return  super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.emails_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.emails_create) {
+            Intent a = new Intent(EmailsActivity.this, CreateEmailActivity.class);
+            startActivity(a);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -87,6 +123,10 @@ public class EmailsActivity extends AppCompatActivity {
                 Intent j = new Intent(EmailsActivity.this, ContactActivity.class);
                 startActivity(j);
                 break;
+            case id.imageView:
+                Intent k = new Intent(EmailsActivity.this, ProfileActivity.class);
+                startActivity(k);
+                break;
         }
 
     }
@@ -119,5 +159,28 @@ public class EmailsActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_emails) {
+            // Handle the camera action
+        } else if (id == R.id.nav_contacts) {
+            Intent c = new Intent(EmailsActivity.this, ContactsActivity.class);
+            startActivity(c);
+        } else if (id == R.id.nav_folders) {
+            Intent d = new Intent(EmailsActivity.this, FoldersActivity.class);
+            startActivity(d);
+        } else if (id == R.id.nav_settings) {
+            Intent f = new Intent(EmailsActivity.this, SettingsActivity.class);
+            startActivity(f);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
