c5bed5b7f96dda70bba1248014ed5368c0f5f751
Despot <jovan1998despotov@gmail.com>
2019-04-02 19:55:01 +0200

    Contacts activiti navigation drawer gotov

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/ContactsActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/ContactsActivity.java
@@ -1,12 +1,20 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.pmsu_2019_projekat.R;
 
-public class ContactsActivity extends AppCompatActivity {
+public class ContactsActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,46 @@ public class ContactsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle("Toolbar");
         toolbar.setSubtitle("Contacts Activity");
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setHomeButtonEnabled(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.contacts_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.contacts_create) {
+            Intent a = new Intent(ContactsActivity.this, CreateContactActivity.class);
+            startActivity(a);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -47,5 +95,26 @@ public class ContactsActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_contacts) {
+        } else if (id == R.id.nav_folders) {
+            Intent c = new Intent(ContactsActivity.this, ContactsActivity.class);
+            startActivity(c);
+        } else if (id == R.id.nav_emails) {
+            Intent d = new Intent(ContactsActivity.this, FoldersActivity.class);
+            startActivity(d);
+        } else if (id == R.id.nav_settings) {
+            Intent f = new Intent(ContactsActivity.this, SettingsActivity.class);
+            startActivity(f);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateContactActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateContactActivity.java
@@ -1,10 +1,13 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.pmsu_2019_projekat.R;
 
@@ -19,6 +22,8 @@ public class CreateContactActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle("Toolbar");
         toolbar.setSubtitle("Create Contact Activity");
+        ActionBar actionbar = getSupportActionBar ();
+        actionbar.setDisplayHomeAsUpEnabled(true) ;
     }
 
     @Override
@@ -26,6 +31,21 @@ public class CreateContactActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.create_contact_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        String message = "";
+        switch (item.getItemId()){
+            case R.id.toolbar_save:
+                message = "Save";
+                break;
+            case R.id.toolbar_cancel:
+                message = "Cancel";
+                break;
+        }
+        Toast.makeText(this, message + "  selected", Toast.LENGTH_LONG).show();
+        return  super.onOptionsItemSelected(item);
     }
     @Override
     protected void onStart() {
