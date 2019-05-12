76c59b4ae977e3da8d09df8d541a4bfa8ca96d23
ljubica_m56 <ljubica.m19@gmail.com>
2019-05-12 14:26:41 +0200

    Login sa SharedPreferences

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/LoginActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.pmsu_2019_projekat.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.pmsu_2019_projekat.R;
@@ -12,19 +15,51 @@ import static com.example.pmsu_2019_projekat.R.*;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText user, pass;
+    Button loginButton;
+    SharedPreferences sharedPreferences;
+    Intent intent;
+
     private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_login);
+
+        user = (EditText) findViewById(id.username_edit);
+        pass = (EditText) findViewById(id.password_edit);
+        loginButton = (Button) findViewById(id.btnStartEmails);
+        sharedPreferences = getSharedPreferences("prefs",MODE_PRIVATE);
+        intent = new Intent(LoginActivity.this,EmailsActivity.class);
+        if(sharedPreferences.contains("username") && sharedPreferences.contains("password")){
+            startActivity(intent);
+        }
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+                if(username.equals("miki") && password.equals("miki123")){
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("username",username);
+                    editor.putString("password",password);
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "Login uspesan",Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Niste uneli dobre informacije",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void btnStartEmailsActivity(View v){
+ /*   public void btnStartEmailsActivity(View v){
         Intent i = new Intent(LoginActivity.this, EmailsActivity.class);
         startActivity(i);
         finish();
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
