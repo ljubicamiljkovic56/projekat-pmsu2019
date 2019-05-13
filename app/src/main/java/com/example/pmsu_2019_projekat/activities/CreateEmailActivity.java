182ce76213231cba8474241d5e9360e9d38acd12
ljubica_m56 <ljubica.m19@gmail.com>
2019-05-13 20:34:09 +0200

    Pokusaj validacije email polja.

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateEmailActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateEmailActivity.java
@@ -1,23 +1,39 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.pmsu_2019_projekat.R;
+
+import java.util.regex.Pattern;
 
 import static com.example.pmsu_2019_projekat.R.*;
 
 public class CreateEmailActivity extends AppCompatActivity {
 
+    private static final Pattern email =
+            Pattern.compile("[a-zA-Z0-9\\+\\.\\_\\%\\+]{1,256}" +
+                    "\\@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +")" );
+
+    private TextInputLayout textTo;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_create_email);
+
+
+        textTo = findViewById(id.email_receiver);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.create_email_toolbar);
         setSupportActionBar(toolbar);
@@ -45,6 +61,28 @@ public class CreateEmailActivity extends AppCompatActivity {
         }
         Toast.makeText(this, message + "  selected", Toast.LENGTH_LONG).show();
         return  super.onOptionsItemSelected(item);
+    }
+
+    private boolean validateEmail(){
+        String emailInput = textTo.getEditText().getText().toString().trim();
+
+        if(emailInput.isEmpty()){
+            textTo.setError("Prazno polje!");
+            return false;
+        }else  if(email.matcher(emailInput).matches()){
+            textTo.setError("Niste dobro uneli email");
+            return false;
+        }else {
+            textTo.setError(null);
+            return true;
+        }
+    }
+    public void validateInput(View v){
+        if(!validateEmail()){
+            return;
+        }
+        String inputT = "Email" + textTo.getEditText().getText().toString();
+        Toast.makeText(this, inputT, Toast.LENGTH_LONG);
     }
 
     @Override
