038a2482a698697aa0b619667a95f86e7548fef9
ljubica_m56 <ljubica.m19@gmail.com>
2019-05-14 19:30:59 +0200

    Validacija kontakta

--- app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateContactActivity.java
+++ app/src/main/java/com/example/pmsu_2019_projekat/activities/CreateContactActivity.java
@@ -1,5 +1,6 @@
 package com.example.pmsu_2019_projekat.activities;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +12,28 @@ import android.widget.Toast;
 
 import com.example.pmsu_2019_projekat.R;
 
+import java.util.regex.Pattern;
+
 public class CreateContactActivity extends AppCompatActivity {
+
+    private static final Pattern email =
+            Pattern.compile("[a-zA-Z0-9\\+\\.\\_\\%\\+]{1,256}" +
+                    "\\@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +")" );
+
+    private TextInputEditText textEmail;
+    private TextInputEditText textName;
+    private TextInputEditText textSurname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact);
+
+
+        textEmail = (TextInputEditText) findViewById(R.id.create_contact_email);
+        textName = (TextInputEditText) findViewById(R.id.create_contact_name);
+        textSurname = (TextInputEditText) findViewById(R.id.create_contact_surname);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.create_contact_toolbar);
         setSupportActionBar(toolbar);
@@ -46,6 +63,57 @@ public class CreateContactActivity extends AppCompatActivity {
         Toast.makeText(this, message + "  selected", Toast.LENGTH_LONG).show();
         return  super.onOptionsItemSelected(item);
     }
+
+    private boolean validateEmail(){
+        String emailInput = textEmail.getText().toString().trim();
+        if(emailInput.isEmpty()){
+            textEmail.setError("Prazno polje!");
+            return false;
+        }else if(email.matcher(emailInput).matches()){
+            textEmail.setError("Niste dobro uneli email");
+            return false;
+        }else {
+            textEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateName(){
+        String nameInput = textName.getText().toString().trim();
+        if(nameInput.isEmpty()){
+            textName.setError("Ime ne sme biti prazno!");
+            return false;
+        }else {
+            textName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateSurname(){
+        String surnameInput = textSurname.getText().toString().trim();
+        if(surnameInput.isEmpty()){
+            textSurname.setError("Prezime ne sme biti prazno!");
+            return false;
+        }else {
+            textSurname.setError(null);
+            return true;
+        }
+    }
+
+
+    public void validateInputContact(MenuItem menuItem){
+        if(!validateEmail() | !validateName() | !validateSurname()){
+            return;
+        }
+        String inputT = "Email" + textEmail.getText().toString();
+        inputT += "\n";
+        inputT += "Name: " + textName.getText().toString();
+        inputT += "\n";
+        inputT += "Surname: " + textSurname.getText().toString();
+        Toast.makeText(this, inputT, Toast.LENGTH_LONG);
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
